fac1a28d694d1a7173aecd550de968cb1575ee98
Karnnan AJ <karnnanaj2004@gmail.com>
2024-08-03 23:12:31 +0530

    inital test

--- cifar10.py
+++ cifar10.py
@@ -10,6 +10,7 @@ from tensorflow.keras.callbacks import EarlyStopping
 physical_devices = tf.config.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
+
 physical_devices = tf.config.list_physical_devices('GPU')
 print("Num GPUs Available: ", len(physical_devices))
 
